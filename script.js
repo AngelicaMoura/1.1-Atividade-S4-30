7a69b3791ae9c0c98849922067fa0aa0216bba87
Angelica Moura <angel_ds7@hotmail.com>
2022-06-20 12:06:32 -0300

    Entrega 1.1

---
+++ script.js
@@ -0,0 +1,15 @@
+let idade=parseInt( prompt("Digite a idade"))
+
+if (idade >60){
+
+alert ("Você está autorizado a comprar ingresso com 50% de desconto")
+
+}
+
+
+
+else(idade >18){
+
+    alert ("Você não está autorizado a comprar o ingresso")
+
+}
